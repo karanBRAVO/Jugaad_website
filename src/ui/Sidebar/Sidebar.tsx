872b799da29ca991410c4c08055ddb3a64cf5f9c
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-03-26 16:50:57 +0530

    updated the sidebar

--- src/ui/Sidebar/Sidebar.tsx
+++ src/ui/Sidebar/Sidebar.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 import Link from "next/link";
+import { IconType } from "react-icons";
 
 // icons
 import {
   FaDiscord,
   FaSquareInstagram,
   FaTelegram,
-  FaTwitter,
+  FaSquareWhatsapp,
 } from "react-icons/fa6";
 
 type Props = {};
 
+interface SidebarLinks {
+  name: string;
+  iconName: IconType;
+  linkTo: string;
+}
+
 const Sidebar = (props: Props) => {
-  const links = [
+  const links: SidebarLinks[] = [
+    {
+      name: "Whatsapp",
+      iconName: FaSquareWhatsapp,
+      linkTo: "https://whatsapp.com",
+    },
+    { name: "Telegram", iconName: FaTelegram, linkTo: "https://telegram.com" },
     {
       name: "Instagram",
       iconName: FaSquareInstagram,
       linkTo: "https://instagram.com",
     },
-    { name: "Twitter", iconName: FaTwitter, linkTo: "https://twitter.com" },
     { name: "Discord", iconName: FaDiscord, linkTo: "https://discord.com" },
-    { name: "Telegram", iconName: FaTelegram, linkTo: "https://telegram.com" },
   ];
 
   return (
@@ -28,7 +39,10 @@ const Sidebar = (props: Props) => {
       <aside className="flex flex-col items-center justify-center bg-white py-2">
         {links.map((link, idx) => (
           <Link href={link.linkTo} key={idx} className="px-3 py-7">
-            <link.iconName className="text-black text-3xl font-black hover:text-red-700 transition-all ease-linear" />
+            <link.iconName
+              className="text-black text-2xl font-black hover:text-red-700 transition-all ease-linear"
+              title={link.name}
+            />
           </Link>
         ))}
       </aside>
