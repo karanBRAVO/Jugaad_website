c1a00bf54ef8d9604a52c36b4c182f53493bb9a3
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-03-26 16:45:24 +0530

    updated the navbar

--- src/ui/Navbar/Navbar.tsx
+++ src/ui/Navbar/Navbar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { Inter } from "next/font/google";
 
 // icons
 import { FaSearch } from "react-icons/fa";
 
+// fonts
+const f_inter_700 = Inter({
+  weight: "700",
+  subsets: ["latin"],
+  display: "swap",
+});
+
 type Props = {};
 
+interface NavbarLinks {
+  name: string;
+  to: string;
+}
+
 const Navbar = (props: Props) => {
-  const links = [
+  const links: NavbarLinks[] = [
     { name: "Home", to: "/" },
     { name: "About", to: "/aboutus" },
     { name: "Activities", to: "/activities" },
@@ -16,27 +29,36 @@ const Navbar = (props: Props) => {
 
   return (
     <>
-      <div className="w-full p-5 flex items-center justify-between border-2 border-solid border-slate-400 border-t-0 border-l-0 border-r-0">
-        <Image
-          src={"/logo.jpg"}
-          alt="Logo"
-          width={100}
-          height={100}
-          className="w-40 aspect-video h-32 rounded-full mx-2"
-        />
+      <div className="w-full p-5 flex items-center justify-around">
+        <div className="flex flex-col items-center justify-center gap-1">
+          <Link href={"/"}>
+            <Image
+              src={"/Logo_head.png"}
+              alt="Logo"
+              width={100}
+              height={100}
+              className="m-1 w-36"
+            />
+            <h1
+              className={`text-white text-xl tracking-wider font-bold uppercase text-center ${f_inter_700.className}`}
+            >
+              Jugaad
+            </h1>
+          </Link>
+        </div>
         <div className="flex flex-row items-center justify-between">
           {links.map((link, idx) => (
             <Link
               href={link.to}
               key={idx}
-              className="uppercase font-semibold text-white mx-5 text-2xl hover:line-through hover:text-red-700 transition-all ease-in-out tracking-normal leading-tight"
+              className={`uppercase font-semibold text-white mx-5 text-2xl hover:line-through hover:text-red-700 transition-all ease-in-out tracking-normal leading-tight ${f_inter_700.className}`}
             >
               {link.name}
             </Link>
           ))}
         </div>
         <FaSearch
-          className="text-white font-black text-5xl cursor-pointer mx-2 hover:text-red-700 hover:scale-110 transition-all ease-in"
+          className="text-white font-black text-3xl cursor-pointer mx-2 hover:text-red-700 hover:scale-110 transition-all ease-in"
           title="Search Jugaad"
         />
       </div>
