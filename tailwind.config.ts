b51286b3e6bb9629a064f5c1b2509021bd61c59d
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-04-17 17:01:07 +0530

    changed the tailwind config and uploaded/removed more images/assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -11,4 +11,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
